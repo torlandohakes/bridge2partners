1c499d8fa1fd56473bec62fea8358c4d12037e62
Torlando Hakes <torlando@craftsmanpainter.com>
2026-04-17 17:41:58 -0400

    fix(security): remove hardcoded secrets that were disabled by github automated security scanners

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyB-NWripd9bhdLYad0ZH_8QVYar0tGdy-8",
-    RESEND_API_KEY: process.env.RESEND_API_KEY || "re_3SyUPTLJ_Gbjcs7R62BtD7gfvfsuFHVEi"
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    RESEND_API_KEY: process.env.RESEND_API_KEY
   },
   images: {
     remotePatterns: [
